7eba8e2341328e11deaa57366fa25a32dd7cb207
ISSEC\alejandro.soler <alejsc.02@gmail.com>
2025-03-18 13:45:58 +0000

    feat: creacion de los js y css relacionados con las lineas, y su puesta en el navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SelectorLineasClases from "./Lineas";
 import "./output.css";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">GEOBUS</div>
-        <div>
+    <nav className="bg-gray-800 p-4 w-full">
+      <div className="w-full flex items-center">
+        {/* Logo alineado a la izquierda */}
+        <div className="text-white text-lg font-bold px-4">GEOBUS</div>
+
+        {/* Contenedor del selector de líneas que ocupa el espacio central */}
+        <div className="flex-grow flex justify-center items-center">
+          <div className="bg-white p-2 rounded-lg shadow-md w-full max-w-3xl flex justify-center">
+            <SelectorLineasClases />
+          </div>
+        </div>
+
+        {/* Enlaces alineados a la derecha */}
+        <div className="flex space-x-4 px-4 h-full">
           <Link
-            to="/"
+            to="/inicio"
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
             Inicio
