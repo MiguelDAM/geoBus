266ac7c7d6ad4d03f28e253ac1907519082352ce
ISSEC\miguel.deleon <milenbri@gmail.com>
2025-03-20 09:24:17 +0000

    change: versión de react-router-dom actualizada para evitar problemas de ejecución

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
-import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
-import MapComponent from "./components/MapComponent";
-import "./styles/output.css";
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useLocation,
+} from 'react-router-dom';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import MapComponent from './components/MapComponent';
+import './styles/output.css';
 
 function AppContent() {
-  const location = useLocation();
+    const location = useLocation();
 
-  return (
-    <div className="App">
-      {/* Renderizar Navbar solo si no estamos en la página de Login */}
-      {location.pathname !== "/admin" && <header className="App-header"><Navbar /></header>}
+    return (
+        <div className="App">
+            {/* Renderizar Navbar solo si no estamos en la página de Login */}
+            {location.pathname !== '/admin' && (
+                <header className="App-header">
+                    <Navbar />
+                </header>
+            )}
 
-      <Routes>
-        <Route path="/admin" element={<Login />} />
-        <Route path="/map" element={<MapComponent />} />
-        <Route path="/contact" element={<div>Contacto</div>} />
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path="/admin" element={<Login />} />
+                <Route path="/map" element={<MapComponent />} />
+                <Route path="/contact" element={<div>Contacto</div>} />
+            </Routes>
+        </div>
+    );
 }
 function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
+    return (
+        <Router>
+            <AppContent />
+        </Router>
+    );
 }
 
 export default App;
