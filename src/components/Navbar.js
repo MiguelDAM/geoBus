a6a43f9405894eba357e40d4b0c99ad30ba039f6
ISSEC\alejandro.soler <alejsc.02@gmail.com>
2025-03-20 13:00:37 +0000

    refactor: mejorar el diseño del Navbar y agregar un ícono de sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import sidebar from "./images/sidebar-icon.svg";
 import SelectorLineasClases from "../pages/Lineas";
 import "../styles/output.css";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 w-full">
-      <div className="w-full flex items-center">
-        <div className="text-white text-lg font-bold px-4">GEOBUS</div>
-
-        <div className="flex-grow flex justify-center items-center">
-          <div className="bg-white p-2 rounded-lg shadow-md w-full max-w-3xl flex justify-center">
+    <nav className="bg-gray-800 p-1 w-full h-17 flex items-center">
+      <div className="w-full flex items-center h-full">
+        <div className="text-white text-2xl font-bold px-4 h-full flex items-center">
+          GEOBUS
+        </div>
+        <div className="flex-grow flex justify-center items-center h-full">
+          <div className="bg-gray-800 p-1 rounded-lg w-full max-w-3xl flex justify-center h-full">
             <SelectorLineasClases />
           </div>
         </div>
-
-        <div className="flex space-x-4 px-4 h-full">
-          <Link
-            to="/inicio"
-            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-          >
-            Inicio
-          </Link>
-          <Link
-            to="/map"
-            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-          >
-            Ver mapa
-          </Link>
-          <Link
-            to="/contact"
-            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-          >
-            Contacto
-          </Link>
+        <div className="flex space-x-4 px-4 h-full items-center cursor-pointer">
+          <img src={sidebar} className="rotate-y-180 h-16"/>
         </div>
       </div>
     </nav>
